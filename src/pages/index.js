2e6036fdd349934f8abd55db7ef8f8f9662505d7
Bill Schnoebelen <Bill@circuitcase.com>
2021-02-04 16:10:04 -0800

    trying original model-viewer

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,8 @@ import Bio from "../components/bio"
 import Img from "gatsby-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import "@google/model-viewer/dist/model-viewer-legacy" //https://github.com/google/model-viewer/pull/1437
+import "@google/model-viewer
+//import "@google/model-viewer/dist/model-viewer-legacy" //https://github.com/google/model-viewer/pull/1437
 
 
 const BlogIndex = ({ data, location }) => {
