230134a2d50150efc43b9588c2415ea2ab2a21e7
Bill Schnoebelen <Bill@circuitcase.com>
2021-02-16 10:40:37 -0800

    added ternary operator to avoid location issue

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -11,7 +11,8 @@ const BlogPostTemplate = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
   const { previous, next } = data
   const { ogimage } = post.frontmatter
-  const href = location.href.replace(/\/$/, "") || {}
+  const href = () => (location ? location.href.replace(/\/$/, "") : {})
+  // const href = location.href.replace(/\/$/, "") || {}
   // const { href } = location
   const ogImagePath = href && ogimage && ogimage.childImageSharp.fixed.src
 
