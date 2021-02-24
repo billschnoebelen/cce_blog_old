850ced857fb380f15cecd287a875b1d06929a610
Bill Schnoebelen <Bill@circuitcase.com>
2021-02-24 09:35:59 -0800

    added google tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,6 +70,17 @@ module.exports = {
         icon: `content/assets/CCElogo.png`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-9Y2DS3PE4R", // Google Analytics / GA
+          //"AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          //"DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+      },
+    },
     `gatsby-plugin-react-helmet`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
